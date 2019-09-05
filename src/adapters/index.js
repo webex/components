408e6bc0ae36d1d73bbbe4a672dc7725da2590f5
Arash Koushkebaghi <arash.koush@gmail.com>
2019-09-05 14:00:44 -0700

    feat(rooms): create adapter interface

--- src/adapters/index.js
+++ src/adapters/index.js
@@ -3,3 +3,4 @@ export {default as PeopleAdapter} from './PeopleAdapter';
 export {default as PeopleJSONAdapter} from './PeopleJSONAdapter';
 export {default as ActivitiesAdapter} from './ActivitiesAdapter';
 export {default as ActivitiesJSONAdapter} from './ActivitiesJSONAdapter';
+export {default as RoomsAdapter} from './RoomsAdapter';
