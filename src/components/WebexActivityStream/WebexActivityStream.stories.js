f114dcb6a2d7402464eab1ecb5927169d8250470
Arash Koushkebaghi <arash.koush@gmail.com>
2019-09-12 00:48:55 -0700

    feat(ActivityStream): implement component  default state

--- src/components/WebexActivityStream/WebexActivityStream.stories.js
+++ src/components/WebexActivityStream/WebexActivityStream.stories.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import {storiesOf} from '@storybook/react';
 
-import RoomsJSONAdapter from '../../adapters/RoomsJSONAdapter';
-import rooms from '../../data/rooms';
+import {ActivitiesJSONAdapter, PeopleJSONAdapter, RoomsJSONAdapter} from '../../adapters';
+import {activities, people, rooms} from '../../data';
 
 import WebexActivityStream from './WebexActivityStream';
 
 // Setup for the stories
 const stories = storiesOf('Webex Activity Stream', module);
-const adapter = new RoomsJSONAdapter(rooms);
+const adapters = {
+  roomsAdapter: new RoomsJSONAdapter(rooms),
+  activitiesAdapter: new ActivitiesJSONAdapter(activities),
+  peopleAdapter: new PeopleJSONAdapter(people),
+};
 
 // Stories
-stories.add('empty group stream', () => <WebexActivityStream roomID="default" adapter={adapter} />);
+stories.add('default', () => <WebexActivityStream roomID="default" adapters={adapters} />);
 
-stories.add('empty 1:1 stream', () => <WebexActivityStream roomID="direct" adapter={adapter} />);
+stories.add('empty group stream', () => <WebexActivityStream roomID="empty-space" adapters={adapters} />);
+
+stories.add('empty 1:1 stream', () => <WebexActivityStream roomID="empty-direct" adapters={adapters} />);
