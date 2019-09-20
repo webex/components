8352d1bf18b1db406b69189e057c0c791d9c413b
Adam Weeks <adweeks@cisco.com>
2019-09-20 12:40:25 -0400

    chore(mocks): update hook mocks for consistency

--- src/components/hooks/__mocks__/useRoom.js
+++ src/components/hooks/__mocks__/useRoom.js
@@ -1,13 +1,7 @@
 export default function useRoom(roomID, adapter) {
-  const rooms = adapter.datasource;
-  const IDs = Object.keys(rooms);
-  let room = null;
-
-  if (IDs.includes(roomID)) {
-    room = rooms[roomID];
-  } else {
+  if (!(roomID in adapter.datasource)) {
     throw new Error(`Could not find room with ID "${roomID}"`);
   }
 
-  return room;
+  return adapter.datasource[roomID];
 }
