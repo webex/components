ce75a4c75fbfb9a55171211277534e57e59929b8
lalli-flores <lalli.flores@gmail.com>
2019-09-21 16:56:10 -0700

    refactor(WebexActivityStream): move time ruler component to its own file

--- src/components/WebexActivityStream/WebexActivityStream.js
+++ src/components/WebexActivityStream/WebexActivityStream.js
@@ -1,7 +1,5 @@
 import React, {Fragment, useRef} from 'react';
 import PropTypes from 'prop-types';
-import {ListSeparator} from '@momentum-ui/react';
-import {format, isToday, isSameWeek, isYesterday} from 'date-fns';
 
 import {RoomType} from '../../adapters/RoomsAdapter';
 import {useActivityStream, useActivityScroll, useOverflowActivities, useRoom} from '../hooks';
@@ -9,45 +7,7 @@ import {PREPEND_ACTIVITIES} from '../hooks/useActivityStream';
 import WebexActivity from '../WebexActivity/WebexActivity';
 
 import Greeting from './Greeting';
-import './WebexActivityStream.scss';
-
-/**
- * Returns a formatted timestamp based on the given date's offset from the current time.
- *
- * Divisor for messages from today display today
- * Divisor for messages from dates from a previous day display as Yesterday
- * Divisor for messages of dates from a previous day of the week (but not yesterday) display as <day of the week>
- * Divisor for messages of dates older than a week from today display as M/D/YY
- *
- * @param {Date} timestamp Date instance to format
- * @returns {string} formattedDate
- */
-export function formatTimeRulerText(timestamp) {
-  let formattedDate;
-
-  if (isToday(timestamp)) {
-    formattedDate = 'today';
-  } else if (isYesterday(timestamp)) {
-    // Yesterday
-    formattedDate = 'Yesterday';
-  } else if (isSameWeek(timestamp, new Date())) {
-    // Monday
-    formattedDate = format(timestamp, 'iiii');
-  } else {
-    // 1/1/2020
-    formattedDate = format(timestamp, 'P');
-  }
-
-  return formattedDate;
-}
-
-export function TimeRuler({text}) {
-  return <ListSeparator className="time-ruler" role="listitem" text={text} />;
-}
-
-TimeRuler.propTypes = {
-  text: PropTypes.string.isRequired,
-};
+import TimeRuler from './TimeRuler';
 
 export default function WebexActivityStream({roomID}) {
   const [activitiesData, dispatch] = useActivityStream(roomID);
@@ -64,7 +24,7 @@ export default function WebexActivityStream({roomID}) {
   const activities = activitiesData.map((activity) => {
     // If the activity is an object with a date property, it is a time ruler
     const activityComponent = activity.date ? (
-      <TimeRuler key={activity.date.toString()} text={formatTimeRulerText(new Date(activity.date))} />
+      <TimeRuler key={activity.date.toString()} date={activity.date} />
     ) : (
       <WebexActivity key={activity} activityID={activity} />
     );
