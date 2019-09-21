04e9ad64deb6661562f9ba28e7a8d111dceca080
Arash Koushkebaghi <arash.koush@gmail.com>
2019-09-21 15:45:44 -0400

    fix(components): update index file to export all components

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,4 @@
 export {default as WebexAvatar} from './WebexAvatar/WebexAvatar';
 export {default as WebexActivity} from './WebexActivity/WebexActivity';
+export {default as WebexActivityStream} from './WebexActivityStream/WebexActivityStream';
+export {default as WebexDataProvider, AdapterContext} from './WebexDataProvider/WebexDataProvider';
