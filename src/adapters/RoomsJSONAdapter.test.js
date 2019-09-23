9b2fd45eb8eb4df90b9919cf02d133fe3c556adc
lalli-flores <lalli.flores@gmail.com>
2019-09-23 11:25:03 -0700

    feat(RoomsJSONAdapter): implement get previous activities chunking

--- src/adapters/RoomsJSONAdapter.test.js
+++ src/adapters/RoomsJSONAdapter.test.js
@@ -43,37 +43,6 @@ describe('Rooms JSON Adapter Interface', () => {
     );
   });
 
-  test('getPreviousRoomActivities() returns an observable', () => {
-    expect(rxjs.isObservable(roomsJSONAdapter.getPreviousRoomActivities())).toBeTruthy();
-  });
-
-  test('getPreviousRoomActivities() returns an array of previous activity IDs', (done) => {
-    roomsJSONAdapter.getPreviousRoomActivities(roomID).subscribe((data) => {
-      expect(data).toEqual(rooms[`${roomID}-activities`]);
-      done();
-    });
-  });
-
-  test('getPreviousRoomActivities() returns an observable to an empty array for a given wrong room ID', (done) => {
-    const wrongRoomActivitiesID = 'wrongRoomActivitiesID';
-
-    roomsJSONAdapter.getPreviousRoomActivities(wrongRoomActivitiesID).subscribe((data) => {
-      expect(data).toEqual([]);
-      done();
-    });
-  });
-
-  test('getPreviousRoomActivities() completes the observable', (done) => {
-    roomsJSONAdapter.getPreviousRoomActivities(roomID).subscribe(
-      () => {},
-      () => {},
-      () => {
-        expect(true).toBeTruthy();
-        done();
-      }
-    );
-  });
-
   test('getRoomActivities() returns an observable', () => {
     expect(rxjs.isObservable(roomsJSONAdapter.getRoomActivities())).toBeTruthy();
   });
@@ -112,6 +81,53 @@ describe('Rooms JSON Adapter Interface', () => {
         done();
       }
     );
+  });
+
+  test('getPreviousRoomActivities() returns an array of previous activity IDs', (done) => {
+    roomsJSONAdapter.getPreviousRoomActivities(roomID).subscribe((data) => {
+      expect(data).toEqual(rooms[`${roomID}-previous-activities`]);
+      done();
+    });
+  });
+
+  test('getPreviousRoomActivities() returns error on wrong room ID', (done) => {
+    const wrongRoomActivitiesID = 'wrongRoomActivitiesID';
+    const message = 'Could not find activities for room wrongRoomActivitiesID. Make sure room ID is valid!';
+
+    roomsJSONAdapter.getPreviousRoomActivities(wrongRoomActivitiesID).subscribe(
+      () => {},
+      (error) => {
+        expect(error.message).toEqual(message);
+        done();
+      }
+    );
+  });
+
+  test('getPreviousRoomActivities() completes the observable when there is no more data', (done) => {
+    // Set last searched index to last element in data
+    roomsJSONAdapter.lastDataIndex[roomID] = rooms[`${roomID}-previous-activities`].length;
+
+    roomsJSONAdapter.getPreviousRoomActivities(roomID).subscribe(
+      () => {},
+      () => {},
+      () => {
+        expect(true).toBeTruthy();
+        done();
+      }
+    );
+  });
+
+  test('hasMoreActivities() returns true if room has more activities to load', () => {
+    roomsJSONAdapter.lastDataIndex[roomID] = 2; // Arbitrary small number
+
+    expect(roomsJSONAdapter.hasMoreActivities(roomID)).toBeTruthy();
+  });
+
+  test('hasMoreActivities() returns false if the room has no more activities to load', () => {
+    // Larger than the total
+    roomsJSONAdapter.lastDataIndex[roomID] = rooms[`${roomID}-previous-activities`].length + 1;
+
+    expect(roomsJSONAdapter.hasMoreActivities(roomID)).toBeFalsy();
   });
 
   afterEach(() => {
