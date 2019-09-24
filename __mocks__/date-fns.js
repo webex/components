5900f6bbda20cf82e836c54d85f7facc5d40eb61
lalli-flores <lalli.flores@gmail.com>
2019-09-23 19:43:16 -0700

    refactor(date-fns): remove Jest mock

--- __mocks__/date-fns.js
+++
@@ -1,4 +0,0 @@
-export const isToday = (time) => time.dateString === 'today';
-export const isYesterday = (time) => time.dateString === 'yesterday';
-export const isSameWeek = (time1) => time1.dateString === 'sameWeek'; // ignore the second argument
-export const format = (time, pattern) => `${time} ${pattern}`;
