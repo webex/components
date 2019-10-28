77ed50ef239b0fe7887246140ff5e233290380de
lalli-flores <lalli.flores@gmail.com>
2019-10-28 15:28:38 -0700

    feat(adapters): remove adapter interfaces

--- src/adapters/index.js
+++ src/adapters/index.js
@@ -1,8 +1,4 @@
-export {default as WebexAdapter} from './WebexAdapter';
 export {default as WebexJSONAdapter} from './WebexJSONAdapter';
-export {default as PeopleAdapter} from './PeopleAdapter';
 export {default as PeopleJSONAdapter} from './PeopleJSONAdapter';
-export {default as ActivitiesAdapter} from './ActivitiesAdapter';
 export {default as ActivitiesJSONAdapter} from './ActivitiesJSONAdapter';
-export {default as RoomsAdapter} from './RoomsAdapter';
 export {default as RoomsJSONAdapter} from './RoomsJSONAdapter';
