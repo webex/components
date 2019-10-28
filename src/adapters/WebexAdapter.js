916b77b3cb6aba6f83d0c375e34023844541b6e6
lalli-flores <lalli.flores@gmail.com>
2019-10-28 15:28:38 -0700

    feat(adapters): remove adapter interfaces

--- src/adapters/WebexAdapter.js
+++
@@ -1,9 +0,0 @@
-export default class WebexAdapter {
-  /**
-   * Creates a new instance of the WebexAdapter.
-   * @param {Object} datasource The primary datasource the adapter will be using.
-   */
-  constructor(datasource) {
-    this.datasource = datasource;
-  }
-}
