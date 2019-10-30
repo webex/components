1f5c4c3ae24248d47aec4676fb939f91bd8ca1e8
lalli-flores <lalli.flores@gmail.com>
2019-10-29 19:09:25 -0700

    refactor(WebexActivityStream): move greeting component on its own file

---
+++ src/components/WebexActivityStream/Greeting.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import Greeting, {GreetingSpaceSVG, GreetingDirectSVG} from './Greeting';
+
+describe('Activity Stream', () => {
+  describe('Greeting Space SVG snapshot', () => {
+    test('matches with greeting space SVG', () => {
+      expect(shallow(<GreetingSpaceSVG />)).toMatchSnapshot();
+    });
+  });
+
+  describe('Greeting 1:1 SVG snapshot', () => {
+    test('matches with greeting 1:1 SVG', () => {
+      expect(shallow(<GreetingDirectSVG />)).toMatchSnapshot();
+    });
+  });
+
+  describe('Greeting component snapshot', () => {
+    test('matches with empty space', () => {
+      expect(shallow(<Greeting personName="" />)).toMatchSnapshot();
+    });
+
+    test('matches with empty 1:1', () => {
+      expect(shallow(<Greeting personName="personName" />)).toMatchSnapshot();
+    });
+  });
+});
