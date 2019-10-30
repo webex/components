c5f10beb5a4131717cf1911a567a9bc14c384a72
lalli-flores <lalli.flores@gmail.com>
2019-10-30 14:27:15 -0700

    test(jest): set mock date for Aug 1, 2019

--- .storybook/setupTests.js
+++ .storybook/setupTests.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import Enzyme, {shallow, render, mount} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import MockDate from 'mockdate';
 import * as rxjs from 'rxjs';
 
 import jsonData from '../src/data';
+
+// Fix date-time for tests
+process.env.TZ = 'GMT';
+MockDate.set('2019-08-01T22:00:00.000Z');
 
 // React 16 Enzyme adapter
 Enzyme.configure({adapter: new Adapter()});
@@ -13,16 +18,6 @@ global.shallow = shallow;
 global.render = render;
 global.mount = mount;
 global.rxjs = rxjs;
-global.Date = class extends Date {
-  constructor(date) {
-    super();
-    this.dateString = date || 'now';
-  }
-
-  toString() {
-    return this.dateString;
-  }
-};
 
 // Mock react `useContext` method globally to return mock json object for every test module
 jest.spyOn(React, 'useContext').mockImplementation((context) => {
