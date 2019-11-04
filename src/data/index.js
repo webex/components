2fb36acb0cf66cf814f262209c888ecd0f969bcf
Adam Weeks <adweeks@cisco.com>
2019-11-04 16:46:40 -0500

    feat(data): add meetings data

--- src/data/index.js
+++ src/data/index.js
@@ -1,6 +1,7 @@
-import people from './people';
 import activities from './activities';
+import meetings from './meetings';
+import people from './people';
 import rooms from './rooms';
 
 // Export all data as one JSON object
-export default {activities, people, rooms};
+export default {activities, meetings, people, rooms};
