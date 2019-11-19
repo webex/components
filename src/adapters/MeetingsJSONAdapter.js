f6b7b98eb3abde369ddbdf317f87202f4262cc5e
lalli-flores <lalli.flores@gmail.com>
2019-11-18 23:27:25 -0800

    feat(MeetingsJSONAdapter): add mute-audio meeting control

--- src/adapters/MeetingsJSONAdapter.js
+++ src/adapters/MeetingsJSONAdapter.js
@@ -1,6 +1,10 @@
-import {from, Observable} from 'rxjs';
-import {flatMap, map} from 'rxjs/operators';
-import {MeetingsAdapter} from '@webex/component-adapter-interfaces';
+import {concat, from, fromEvent, Observable} from 'rxjs';
+import {filter, flatMap, map} from 'rxjs/operators';
+import {MeetingsAdapter, MeetingControlState} from '@webex/component-adapter-interfaces';
+
+// Defined meeting controls in Meetings JSON Adapter
+export const MUTE_AUDIO_CONTROL = 'mute-audio';
+
 /**
  * @typedef MeetingsJSON
  * @param {object} datasource An object that contains a set of meetings keyed by ID.
@@ -22,28 +26,29 @@ import {MeetingsAdapter} from '@webex/component-adapter-interfaces';
  */
 
 /**
- * Implements the MeetingsAdapter interface with a JSON object as its datasource. See @MeetingsJSON
+ * Implements the MeetingsAdapter interface with a JSON object as its datasource.
+ *
+ * @see {@link MeetingsJSON} for example datasource.
+ * @class
+ * @implements {MeetingsAdapter}
  */
 export default class MeetingsJSONAdapter extends MeetingsAdapter {
   /**
-   * Returns a MediaStream object obtained from local user media.
-   * @returns {MediaStream}
+   * Creates a new MeetingJSONAdapter.
+   *
+   * @param {object} datasource An object that contains a set of meetings keyed by ID.
+   * @hideconstructor
    */
-  async getLocalVideo() {
-    const constraints = {
-      video: true,
-      audio: false,
+  constructor(datasource) {
+    super(datasource);
+
+    this.meetingControls = {};
+
+    this.meetingControls[MUTE_AUDIO_CONTROL] = {
+      ID: MUTE_AUDIO_CONTROL,
+      action: this.toggleMuteAudio.bind(this),
+      display: this.muteAudioControl.bind(this),
     };
-    let stream;
-
-    try {
-      stream = await navigator.mediaDevices.getUserMedia(constraints);
-    } catch (reason) {
-      // eslint-disable-next-line no-console
-      console.error('Meetings JSON adapter can not display the user local stream', reason);
-    }
-
-    return stream;
   }
 
   /**
@@ -66,14 +71,104 @@ export default class MeetingsJSONAdapter extends MeetingsAdapter {
       observer.complete();
     });
 
-    return getMeeting$.pipe(
+    // Attach local media stream if meeting `localVideo` property is not `null`.
+    // We can not attach the MediaStream object in a JSON module, the work needs
+    // to be done here.
+    const getMeetingWithLocalMedia$ = getMeeting$.pipe(
       flatMap((meeting) =>
-        // Attach the localMedia stream if meeting localVideo
-        // property is not `null`. Since we can not attach the
-        // MediaStream object in out JSON module, the work needs
-        // to be done here.
         from(this.getLocalVideo()).pipe(map((localVideo) => (meeting.localVideo ? {...meeting, localVideo} : meeting)))
       )
     );
+
+    // Send updates on the meeting when a mute event is triggered
+    const muteEvent$ = fromEvent(document, MUTE_AUDIO_CONTROL).pipe(
+      filter((event) => event.detail.ID === ID),
+      map((event) => event.detail)
+    );
+
+    return concat(getMeetingWithLocalMedia$, muteEvent$);
+  }
+
+  /**
+   * Returns a MediaStream object obtained from local user media.
+   * @returns {MediaStream}
+   */
+  async getLocalVideo() {
+    const constraints = {
+      video: true,
+      audio: false,
+    };
+    let stream;
+
+    try {
+      stream = await navigator.mediaDevices.getUserMedia(constraints);
+    } catch (reason) {
+      // eslint-disable-next-line no-console
+      console.error('Meetings JSON adapter can not display the user local stream', reason);
+    }
+
+    return stream;
+  }
+
+  /**
+   * Returns an observable that emits the display data of a meeting control.
+   *
+   * @param {string} ID ID of the meeting for which to update display
+   * @returns {Observable<MeetingControlDisplay>}
+   * @memberof MeetingJSONAdapter
+   * @private
+   */
+  muteAudioControl(ID) {
+    const unmuted = {
+      ID: MUTE_AUDIO_CONTROL,
+      icon: 'microphone',
+      tooltip: 'Mute',
+      state: MeetingControlState.INACTIVE,
+    };
+    const muted = {
+      ID: MUTE_AUDIO_CONTROL,
+      icon: 'microphone-muted',
+      tooltip: 'Unmute',
+      state: MeetingControlState.ACTIVE,
+    };
+
+    const default$ = Observable.create((observer) => {
+      const meeting = this.datasource[ID];
+
+      if (meeting) {
+        observer.next(meeting.localAudio.muted ? muted : unmuted);
+      } else {
+        observer.error(new Error(`Could not find meeting with ID "${ID}"`));
+      }
+
+      observer.complete();
+    });
+
+    const muteEvent$ = fromEvent(document, MUTE_AUDIO_CONTROL).pipe(
+      filter((event) => event.detail.ID === ID),
+      map((event) => (event.detail.localAudio.muted ? muted : unmuted))
+    );
+
+    return concat(default$, muteEvent$);
+  }
+
+  /**
+   * Toggles muting the local audio media stream track.
+   * Used by "mute-audio" meeting control.
+   *
+   * @param {string} ID  ID of the meeting for which to mute local audio.
+   * @memberof MeetingsJSONAdapter
+   * @private
+   */
+  toggleMuteAudio(ID) {
+    if (this.datasource[ID]) {
+      const meeting = {...this.datasource[ID]}; // Copy meeting to modify mute
+      const muteEvent = new CustomEvent(MUTE_AUDIO_CONTROL, {detail: meeting});
+
+      if (meeting.localAudio) {
+        meeting.localAudio.muted = !meeting.localAudio.muted;
+        document.dispatchEvent(muteEvent);
+      }
+    }
   }
 }
