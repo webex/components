fd60ceef65596d2646b64909d396ed006c78c2a1
lalli-flores <lalli.flores@gmail.com>
2019-11-18 23:27:25 -0800

    feat(MeetingsJSONAdapter): add mute-audio meeting control

--- src/adapters/MeetingsJSONAdapter.test.js
+++ src/adapters/MeetingsJSONAdapter.test.js
@@ -1,3 +1,6 @@
+import {from} from 'rxjs';
+import {tap, skip} from 'rxjs/operators';
+
 import meetings from './../data/meetings';
 import MeetingsJSONAdapter from './MeetingsJSONAdapter';
 
@@ -10,44 +13,112 @@ describe('Meetings JSON Adapter', () => {
     meetingsJSONAdapter.getLocalVideo = jest.fn(() => Promise.resolve('mock-stream'));
   });
 
-  test('getMeeting() returns an observable', () => {
-    expect(rxjs.isObservable(meetingsJSONAdapter.getMeeting())).toBeTruthy();
-  });
+  describe('getMeeting()', () => {
+    test('returns an observable', () => {
+      expect(rxjs.isObservable(meetingsJSONAdapter.getMeeting())).toBeTruthy();
+    });
 
-  test('getMeeting() returns a meeting data', (done) => {
-    meetingsJSONAdapter.getMeeting(meetingID).subscribe((data) => {
-      expect(data).toEqual(meetings[meetingID]);
-      done();
+    test('returns meeting data', (done) => {
+      meetingsJSONAdapter.getMeeting(meetingID).subscribe((data) => {
+        expect(data).toEqual(meetings[meetingID]);
+        done();
+      });
+    });
+
+    test('renders the local media if localVideo property is defined', (done) => {
+      meetingsJSONAdapter.getMeeting('localVideo').subscribe((data) => {
+        expect(data.localVideo).toEqual('mock-stream');
+        done();
+      });
+    });
+
+    test('returns meeting data from mute event', (done) => {
+      meetingID = 'localAudio';
+
+      meetingsJSONAdapter
+        .getMeeting(meetingID)
+        .pipe(tap(() => meetingsJSONAdapter.toggleMuteAudio(meetingID)))
+        .subscribe((meeting) => {
+          expect(meeting.localAudio.muted).toBeTruthy();
+          done();
+        });
+    });
+
+    test('throws a proper error message', (done) => {
+      const wrongMeetingID = 'wrongMeetingID';
+
+      meetingsJSONAdapter.getMeeting(wrongMeetingID).subscribe(
+        () => {},
+        (error) => {
+          expect(error.message).toBe(`Could not find meeting with ID "${wrongMeetingID}"`);
+          done();
+        }
+      );
     });
   });
 
-  test('getMeeting() throws a proper error message', (done) => {
-    const wrongMeetingID = 'wrongMeetingID';
+  describe('muteAudioControl() returns', () => {
+    beforeEach(() => {
+      meetingID = 'localAudio';
+    });
 
-    meetingsJSONAdapter.getMeeting(wrongMeetingID).subscribe(
-      () => {},
-      (error) => {
-        expect(error.message).toBe(`Could not find meeting with ID "${wrongMeetingID}"`);
-        done();
-      }
-    );
+    test('active control display values', (done) => {
+      rxjs.fromEvent = jest.fn(() => from([{detail: meetings[meetingID]}]));
+
+      meetingsJSONAdapter
+        .muteAudioControl(meetingID)
+        .pipe(skip(1)) // Skip the "default" emission
+        .subscribe((display) => {
+          expect(display).toMatchObject({
+            ID: 'mute-audio',
+            icon: 'microphone-muted',
+            tooltip: 'Unmute',
+            state: 'active',
+          });
+          done();
+        });
+    });
+
+    test('inactive control display values', (done) => {
+      rxjs.fromEvent = jest.fn(() => {
+        const meeting = meetings[meetingID];
+
+        meeting.localAudio.muted = false; // Local audio is already muted
+
+        return from([{detail: meeting}]);
+      });
+
+      meetingsJSONAdapter
+        .muteAudioControl(meetingID)
+        .pipe(skip(1)) // Skip the "default" emission
+        .subscribe((display) => {
+          expect(display).toMatchObject({
+            ID: 'mute-audio',
+            icon: 'microphone',
+            tooltip: 'Mute',
+            state: 'inactive',
+          });
+          done();
+        });
+    });
+
+    test('error on invalid meeting ID', (done) => {
+      meetingsJSONAdapter.muteAudioControl('invalid').subscribe(
+        () => {},
+        (error) => {
+          expect(error.message).toEqual('Could not find meeting with ID "invalid"');
+          done();
+        }
+      );
+    });
   });
 
-  test('getMeeting() completes the observable', (done) => {
-    meetingsJSONAdapter.getMeeting(meetingID).subscribe(
-      () => {},
-      () => {},
-      () => {
-        expect(true).toBeTruthy();
-        done();
-      }
-    );
-  });
+  describe('toggleMuteAudio()', () => {
+    test('dispatches a "mute-audio" event', () => {
+      meetingID = 'localAudio';
 
-  test('getMeeting() renders the local media if localVideo property is defined', (done) => {
-    meetingsJSONAdapter.getMeeting('localVideo').subscribe((data) => {
-      expect(data.localVideo).toEqual('mock-stream');
-      done();
+      meetingsJSONAdapter.toggleMuteAudio(meetingID);
+      expect(global.document.dispatchEvent).toHaveBeenCalled();
     });
   });
 
