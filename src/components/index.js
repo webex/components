c8bb5266ccd5f8df72a6ec7874d2aa49d4655343
Arash Koushkebaghi <arash.koush@gmail.com>
2019-11-21 16:13:26 -0800

    feat(WebexLocalMedia): implement a component

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,5 @@ export {default as WebexAvatar} from './WebexAvatar/WebexAvatar';
 export {default as WebexActivity} from './WebexActivity/WebexActivity';
 export {default as WebexActivityStream} from './WebexActivityStream/WebexActivityStream';
 export {default as WebexDataProvider, AdapterContext} from './WebexDataProvider/WebexDataProvider';
+export {default as WebexLocalMedia} from './WebexLocalMedia/WebexLocalMedia';
 export {default as WebexMeetingInfo} from './WebexMeetingInfo/WebexMeetingInfo';
