5c895880681996aaab2cd90cfc46457062f62190
lalli-flores <lalli.flores@gmail.com>
2019-11-22 17:16:32 -0800

    feat(WebexMeetingControls): implement HOC

---
+++ src/components/WebexMeetingControl/WebexMeetingControls.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const MeetingContext = React.createContext();
+
+/**
+ * WebexMeetingControls is a higher-order component that pass a meeting
+ * context to several WebexMeetingControl components.
+ *
+ * @param {object} props
+ * @returns {object} JSX of the component
+ */
+export default function WebexMeetingControls({meetingID, children}) {
+  return <MeetingContext.Provider value={meetingID}>{children}</MeetingContext.Provider>;
+}
+
+WebexMeetingControls.propTypes = {
+  meetingID: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};
