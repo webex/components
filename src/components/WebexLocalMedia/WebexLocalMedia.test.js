1e0c1f6994fe172354dfc0fff38e84a57c76db53
lalli-flores <lalli.flores@gmail.com>
2019-12-13 15:36:59 -0800

    feat(LocalMedia): use getMe hook instead of personID prop

--- src/components/WebexLocalMedia/WebexLocalMedia.test.js
+++ src/components/WebexLocalMedia/WebexLocalMedia.test.js
@@ -2,17 +2,18 @@ import React from 'react';
 
 import WebexLocalMedia from './WebexLocalMedia';
 
+jest.mock('../hooks/useMe');
 jest.mock('../hooks/useMeeting');
 jest.mock('../hooks/useVideo');
 
 describe('Webex Local Media component', () => {
   describe('snapshot', () => {
     test('matches snapshot of local video', () => {
-      expect(shallow(<WebexLocalMedia meetingID="localVideo" personID="default" />)).toMatchSnapshot();
+      expect(shallow(<WebexLocalMedia meetingID="localVideo" />)).toMatchSnapshot();
     });
 
     test('matches snapshot of disabled local video', () => {
-      expect(shallow(<WebexLocalMedia meetingID="scheduledMeeting" personID="default" />)).toMatchSnapshot();
+      expect(shallow(<WebexLocalMedia meetingID="scheduledMeeting" />)).toMatchSnapshot();
     });
   });
 });
