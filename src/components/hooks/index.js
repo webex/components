c63de3b0c093143c9138c6c39bf18d02ebe1e31c
Taymoor Khan <tmkhan.k2@gmail.com>
2019-12-17 16:44:43 -0800

    feat(hooks): replace useStream with useVideo
    Co-authored-by: Arash koushkebaghi <arash.koush@gmail.com>

--- src/components/hooks/index.js
+++ src/components/hooks/index.js
@@ -7,4 +7,4 @@ export {default as useMeetingControl} from './useMeetingControl';
 export {default as usePerson} from './usePerson';
 export {default as useOverflowActivities} from './useOverflowActivities';
 export {default as useRoom} from './useRoom';
-export {default as useVideo} from './useVideo';
+export {default as useStream} from './useStream';
