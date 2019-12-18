ade41d067c8c6ff02319c4f62b8335903dd322a0
lalli-flores <lalli.flores@gmail.com>
2019-12-18 12:41:25 -0800

    fix(LocalMedia): display spinner while user ID is retrieved

--- src/components/WebexLocalMedia/WebexLocalMedia.js
+++ src/components/WebexLocalMedia/WebexLocalMedia.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {Spinner} from '@momentum-ui/react';
 
 import {WebexAvatar} from '../';
 import {useMe, useMeeting, useStream} from '../hooks';
@@ -16,12 +17,9 @@ export default function WebexLocalMedia({meetingID}) {
   const {localVideo} = useMeeting(meetingID);
   const {ID} = useMe();
   const videoRef = useStream(localVideo);
+  const disabledVideo = ID ? <WebexAvatar personID={ID} /> : <Spinner />;
 
-  return (
-    <div className="local-media">
-      {localVideo ? <video ref={videoRef} playsInline /> : <WebexAvatar personID={ID} />}
-    </div>
-  );
+  return <div className="local-media">{localVideo ? <video ref={videoRef} playsInline /> : disabledVideo}</div>;
 }
 
 WebexLocalMedia.propTypes = {
