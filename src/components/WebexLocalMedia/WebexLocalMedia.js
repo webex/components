8126be7c1567647d32013037089b3daf7fe0cb74
lalli-flores <lalli.flores@gmail.com>
2020-02-24 09:39:40 -0800

    fix(LocalMedia): add 'no-media' CSS class when video is disabled

--- src/components/WebexLocalMedia/WebexLocalMedia.js
+++ src/components/WebexLocalMedia/WebexLocalMedia.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import {Spinner} from '@momentum-ui/react';
 
 import {WebexAvatar} from '../';
@@ -17,11 +18,15 @@ export default function WebexLocalMedia({meetingID}) {
   const {localVideo} = useMeeting(meetingID);
   const {ID} = useMe();
   const videoRef = useStream(localVideo);
+
+  const cssClasses = classNames({
+    'local-media': true,
+    'no-media': localVideo === null,
+  });
+
   const disabledVideo = ID ? <WebexAvatar personID={ID} displayStatus={false} /> : <Spinner />;
 
-  return (
-    <div className="local-media">{localVideo ? <video ref={videoRef} playsInline autoPlay /> : disabledVideo}</div>
-  );
+  return <div className={cssClasses}>{localVideo ? <video ref={videoRef} playsInline autoPlay /> : disabledVideo}</div>;
 }
 
 WebexLocalMedia.propTypes = {
