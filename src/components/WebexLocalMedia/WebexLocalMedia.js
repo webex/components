c80838ae51f69d871658827091a3c20b0aec26ed
Adam Weeks <adweeks@cisco.com>
2020-03-20 10:07:24 -0400

    feat(WebexLocalMedia): add className prop

--- src/components/WebexLocalMedia/WebexLocalMedia.js
+++ src/components/WebexLocalMedia/WebexLocalMedia.js
@@ -16,30 +16,33 @@ import './WebexLocalMedia.scss';
  * @param {string} props.meetingID  ID of the meeting from which to obtain local media
  * @returns {Object} JSX of the component
  */
-export default function WebexLocalMedia({meetingID}) {
+export default function WebexLocalMedia({className, meetingID}) {
   const [mediaRef, {width}] = useElementDimensions();
   const {localVideo} = useMeeting(meetingID);
   const {ID} = useMe();
   const videoRef = useStream(localVideo);
 
-  const classBaseName = 'local-media';
-  const classObjects = {};
-
-  classObjects[`${WEBEX_COMPONENTS_CLASS_PREFIX}-${classBaseName}`] = true;
-  classObjects[`${WEBEX_COMPONENTS_CLASS_PREFIX}-${classBaseName}-desktop`] = width >= PHONE_LARGE;
-  classObjects[`${WEBEX_COMPONENTS_CLASS_PREFIX}-no-media`] = localVideo === null;
-
-  const cssClasses = classNames(classObjects);
-
+  const classBaseName = `${WEBEX_COMPONENTS_CLASS_PREFIX}-local-media`;
+  const mainClasses = {
+    [classBaseName]: true,
+    [`${classBaseName}-desktop`]: width >= PHONE_LARGE,
+    [`${classBaseName}-no-media`]: localVideo === null,
+    [className]: !!className,
+  };
   const disabledVideo = ID ? <WebexAvatar personID={ID} displayStatus={false} /> : <Spinner />;
 
   return (
-    <div ref={mediaRef} className={cssClasses}>
+    <div ref={mediaRef} className={classNames(mainClasses)}>
       {localVideo ? <video ref={videoRef} playsInline autoPlay /> : disabledVideo}
     </div>
   );
 }
 
 WebexLocalMedia.propTypes = {
+  className: PropTypes.string,
   meetingID: PropTypes.string.isRequired,
+};
+
+WebexLocalMedia.defaultProps = {
+  className: '',
 };
