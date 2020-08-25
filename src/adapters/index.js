dd665c4dde3eeafc5b2527de59b77a595dbf06b6
lalli-flores <lalli.flores@gmail.com>
2020-08-25 16:35:32 -0700

    feat(adapters): export JSON adapter as part of distribution

--- src/adapters/index.js
+++ src/adapters/index.js
@@ -1,6 +1,6 @@
 export {default as WebexJSONAdapter} from './WebexJSONAdapter';
 export {default as ActivitiesJSONAdapter} from './ActivitiesJSONAdapter';
 export {default as MeetingsJSONAdapter} from './MeetingsJSONAdapter';
+export {default as MembershipJSONAdapter} from './MembershipJSONAdapter';
 export {default as PeopleJSONAdapter} from './PeopleJSONAdapter';
 export {default as RoomsJSONAdapter} from './RoomsJSONAdapter';
-export {default as MembershipJSONAdapter} from './MembershipJSONAdapter';
