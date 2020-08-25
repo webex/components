608a92dbdeea036bc02e588dbc85c27a316ac4da
Adam Weeks <adweeks@cisco.com>
2020-08-25 14:44:08 -0400

    refactor(hooks): use "members" not "participants"

--- src/components/hooks/__mocks__/useMemberships.js
+++ src/components/hooks/__mocks__/useMemberships.js
@@ -2,11 +2,11 @@ import {useContext} from 'react';
 
 export default function useMemberships(membershipID) {
   const datasource = useContext();
-  let participants = {...datasource.membershipsAdapter[membershipID]};
+  let members = {...datasource.membershipsAdapter[membershipID]};
 
   if (!(membershipID in datasource.membershipsAdapter)) {
-    participants = [];
+    members = [];
   }
 
-  return participants;
+  return members;
 }
