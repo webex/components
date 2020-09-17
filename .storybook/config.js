eee8250a6878c781bf9d39116eef72535e88e67c
Adam Weeks <adweeks@cisco.com>
2020-09-17 11:45:06 -0400

    fix(storybook): load external momentum css

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,5 @@
 import {configure} from '@storybook/react';
+import '@momentum-ui/core/css/momentum-ui.min.css';
 
 function loadStories() {
   const req = require.context('../src', true, /\.stories\.js$/);
