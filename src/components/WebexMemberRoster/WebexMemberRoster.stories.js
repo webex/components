49a2312c3ecf82d4fb1321e04320424be9cc8d8a
Benjamin Golya <golya.benjamin@icloud.com>
2021-03-18 12:14:15 +0200

    docs(Storybook): dropdown for destinationID and destinationType on WebexMemberRoster

--- src/components/WebexMemberRoster/WebexMemberRoster.stories.js
+++ src/components/WebexMemberRoster/WebexMemberRoster.stories.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import {DestinationType} from '@webex/component-adapter-interfaces';
 import WebexMemberRoster from './WebexMemberRoster';
+import Memberships from '../../data/memberships';
+
+const destinationIDs = Object.values(Memberships).map((membership) => membership.destinationID);
 
 export default {
   title: 'Platform/Webex Member Roster',
   component: WebexMemberRoster,
+  argTypes: {
+    destinationID: {
+      control: {
+        type: 'select',
+        options: destinationIDs,
+      },
+    },
+    destinationType: {
+      control: {
+        type: 'select',
+        options: Object.values(DestinationType),
+      },
+    },
+  },
 };
 
 const Template = (args) => <WebexMemberRoster {...args} />;
