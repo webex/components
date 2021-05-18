b4ddf62d8d4efb25c30c3aed9e6565938ec1d748
Catalin Nicolae Lungan <lungancatalin@yahoo.com>
2021-05-18 15:41:17 +0300

    feat(WebexMediaAccess): add microphone access screen

--- src/components/WebexMediaAccess/WebexMediaAccess.jsx
+++ src/components/WebexMediaAccess/WebexMediaAccess.jsx
@@ -4,31 +4,53 @@ import {Icon} from '@momentum-ui/react';
 import WebexMeetingControl from '../WebexMeetingControl/WebexMeetingControl';
 import {WEBEX_COMPONENTS_CLASS_PREFIX} from '../../constants';
 
+const SCREENS = {
+  camera: {
+    icon: 'icon-camera_36',
+    title: 'Allow access to camera',
+    message: 'when your browser asks to let Webex use your camera for this video call',
+    control: 'join-without-camera',
+  },
+  microphone: {
+    icon: 'icon-microphone_36',
+    title: 'Allow access to microphone',
+    message: 'when your browser asks to let Webex use your microphone for this video call',
+    control: 'join-without-microphone',
+  },
+};
+
 /**
  * Webex Media Access component displays a prompt explaining necessary access to media.
  *
  * @param {object} props Data passed to the component
  * @param {string} props.meetingID  ID of the meeting
+ * @param {string} props.media State of media controls
  * @returns {object} JSX of the component
  */
+export default function WebexMediaAccess({meetingID, media}) {
+  const screen = SCREENS[media];
 
-export default function WebexMediaAccess({meetingID}) {
   return (
     <div className={`${WEBEX_COMPONENTS_CLASS_PREFIX}-media-access`}>
-      <Icon name="icon-camera_36" />
-      <h2 className="title">Allow access to camera</h2>
+      <Icon name={screen.icon} />
+      <h2 className="title">{screen.title}</h2>
       <p className="body">
         Select
         {' '}
         <span className="allow">Allow</span>
         {' '}
-        when your browser asks to let Webex use your camera for this video call
+        {screen.message}
       </p>
-      <WebexMeetingControl meetingID={meetingID} type="join-without-camera" />
+      <WebexMeetingControl meetingID={meetingID} type={screen.control} />
     </div>
   );
 }
 
 WebexMediaAccess.propTypes = {
   meetingID: PropTypes.string.isRequired,
+  media: PropTypes.oneOf(['camera', 'microphone']),
+};
+
+WebexMediaAccess.defaultProps = {
+  media: 'camera',
 };
--- src/components/WebexMediaAccess/WebexMediaAccess.stories.js
+++ src/components/WebexMediaAccess/WebexMediaAccess.stories.js
@@ -11,4 +11,11 @@ const Template = (args) => <WebexMediaAccess {...args} />;
 export const Camera = Template.bind({});
 Camera.args = {
   meetingID: 'meeting1',
+  media: 'camera',
+};
+
+export const Microphone = Template.bind({});
+Microphone.args = {
+  meetingID: 'meeting1',
+  media: 'microphone',
 };
