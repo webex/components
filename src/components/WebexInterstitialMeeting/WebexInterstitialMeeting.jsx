865b765804e0ef14fb766319bd04565765167a01
webex-components-publisher <webex-components@cisco.com>
2021-06-16 13:36:10 +0000

    feat(WebexInterstitialMeeting): add  video access dialog in interstial meeting

--- src/components/WebexInterstitialMeeting/WebexInterstitialMeeting.jsx
+++ src/components/WebexInterstitialMeeting/WebexInterstitialMeeting.jsx
@@ -4,8 +4,11 @@ import {Spinner} from '@momentum-ui/react';
 
 import {WEBEX_COMPONENTS_CLASS_PREFIX} from '../../constants';
 
+import Modal from '../generic/Modal/Modal';
 import WebexLocalMedia from '../WebexLocalMedia/WebexLocalMedia';
 import WebexMeetingInfo from '../WebexMeetingInfo/WebexMeetingInfo';
+import WebexMediaAccess from '../WebexMediaAccess/WebexMediaAccess';
+import {useMeeting} from '../hooks';
 
 /**
  * Webex Interstitial component displays the user's local video and
@@ -16,12 +19,19 @@ import WebexMeetingInfo from '../WebexMeetingInfo/WebexMeetingInfo';
  * @returns {object} JSX of the component
  */
 export default function WebexInterstitialMeeting({meetingID}) {
+  const {videoPermission} = useMeeting(meetingID);
+
   return (
     <div className={`${WEBEX_COMPONENTS_CLASS_PREFIX}-interstitial-meeting`}>
       {meetingID ? (
         <>
           <WebexMeetingInfo className="interstitial-meeting-info" meetingID={meetingID} />
           <WebexLocalMedia className="interstitial-media" meetingID={meetingID} mediaType="video" />
+          {videoPermission === 'ASKING' && (
+            <Modal>
+              <WebexMediaAccess meetingID={meetingID} />
+            </Modal>
+          )}
         </>
       ) : (
         <Spinner />
--- src/data/meetings.js
+++ src/data/meetings.js
@@ -14,6 +14,7 @@ export default {
     showRoster: false,
     showSettings: false,
     cameraID: 'cameraID1',
+    videoPermissions: 'ALLOWED',
   },
   meeting2: {
     ID: 'meeting2',
@@ -28,6 +29,7 @@ export default {
     showRoster: false,
     showSettings: false,
     cameraID: 'cameraID2',
+    videoPermission: 'ASKING',
   },
   meeting3: {
     ID: 'meeting3',
@@ -42,6 +44,7 @@ export default {
     showRoster: false,
     showSettings: false,
     cameraID: 'cameraID1',
+    videoPermission: 'ALLOWED',
   },
   meeting4: {
     ID: 'meeting4',
@@ -56,6 +59,7 @@ export default {
     showRoster: false,
     showSettings: false,
     cameraID: 'cameraID1',
+    videoPermission: 'ALLOWED',
   },
   meeting5: {
     ID: 'meeting5',
@@ -70,6 +74,7 @@ export default {
     showRoster: true,
     showSettings: false,
     cameraID: 'cameraID2',
+    videoPermission: 'DENIED',
   },
   meeting6: {
     ID: 'meeting6',
@@ -84,6 +89,7 @@ export default {
     showRoster: false,
     showSettings: false,
     cameraID: 'cameraID1',
+    videoPermission: 'ALLOWED',
   },
   meeting7: {
     ID: 'meeting7',
@@ -98,6 +104,7 @@ export default {
     showRoster: false,
     showSettings: false,
     cameraID: null,
+    videoPermission: null,
   },
   meeting8: {
     ID: null,
@@ -112,6 +119,7 @@ export default {
     showRoster: false,
     showSettings: false,
     cameraID: null,
+    videoPermission: null,
   },
   meeting9: {
     error: true,
