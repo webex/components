8df1810b5696a84730fb04d91c1d25370447ec86
Patricia Mihut <pmihut@cisco.com>
2021-07-20 19:49:41 +0300

    fix(UseMeetingDestination): chain dependent observables createMeeting and getMeeting

--- src/util.js
+++ src/util.js
@@ -1,3 +1,5 @@
+import {Observable} from 'rxjs';
+
 /**
  * Helper function for deep merge on objects.
  *
@@ -16,6 +18,46 @@ export function deepMerge(dest, src) {
   }
 }
 
+/**
+ * Custom rxjs operator for chaining dependent observables.
+ * Usage:
+  ```js
+  obs.pipe(
+    chainWith((lastMessage) => createDependentObservable(lastMessage),
+  );
+  ```
+ * @param {function(lastMessage): Observable} createDependentObservable  Function that is passed the last message emitted by the source observable and returns a new observable
+ * @returns {Observable} observable
+ */
+export function chainWith(createDependentObservable) {
+  return (source) => new Observable((subscriber) => {
+    let lastValue;
+    let subscription;
+
+    subscription = source.subscribe(
+      (value) => {
+        subscriber.next(value);
+        lastValue = value;
+      },
+      (error) => subscriber.error(error),
+      () => {
+        subscription = createDependentObservable(lastValue).subscribe(
+          (value) => subscriber.next(value),
+          (error) => subscriber.error(error),
+          () => subscriber.complete(),
+        );
+      },
+    );
+
+    return () => {
+      if (subscription) {
+        subscription.unsubscribe();
+      }
+    };
+  });
+}
+
 export default {
   deepMerge,
+  chainWith,
 };
