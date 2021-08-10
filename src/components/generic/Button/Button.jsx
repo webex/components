3b49b813082a98ab7258f8cae4345a2c4ad5456f
Patricia Mihut <pmihut@cisco.com>
2021-08-10 19:57:10 +0300

    feat(WebexComponents): create themed Button component

---
+++ src/components/generic/Button/Button.jsx
@@ -0,0 +1,52 @@
+import React, {JSX} from 'react';
+import PropTypes from 'prop-types';
+import webexComponentClasses from '../../helpers';
+
+/**
+ * Button component
+ *
+ * @param {object} props  Data passed to the component
+ * @param {JSX.Element} props.children  List of children
+ * @param {string} [props.className]  Custom CSS class to apply
+ * @param {boolean} props.isDisabled  Flag indicating button disabled
+ * @param {Function} props.onClick  OnClick callback
+ * @param {object} props.style  Inline style object for the component
+ * @param {string} props.title  Tooltip to be displayed
+ * @param {'default'|'join'|'cancel'|'ghost'} [props.type='default']  Button type
+ * @returns {object}  JSX of the element
+ */
+export default function Button({
+  children,
+  className,
+  isDisabled,
+  onClick,
+  title,
+  style,
+  type,
+}) {
+  const cssClasses = webexComponentClasses('button', className, {[`-${type}`]: true});
+
+  return (
+    <button className={cssClasses} disabled={isDisabled} title={title} type="button" onClick={onClick} style={style}>
+      {children}
+    </button>
+  );
+}
+
+Button.propTypes = {
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string,
+  isDisabled: PropTypes.bool,
+  onClick: PropTypes.func.isRequired,
+  title: PropTypes.string,
+  style: PropTypes.shape(),
+  type: PropTypes.string,
+};
+
+Button.defaultProps = {
+  className: '',
+  isDisabled: false,
+  title: '',
+  style: {},
+  type: 'default',
+};
