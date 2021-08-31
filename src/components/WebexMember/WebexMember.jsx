0eae8a16a61de7118d5679f31a609b01823ad824
BenjaminGolya <golya.benjamin@icloud.com>
2021-08-31 13:12:33 +0300

    refactor(WebexMember): use Spinner component instead momentum

--- src/components/WebexMember/WebexMember.jsx
+++ src/components/WebexMember/WebexMember.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {DestinationType} from '@webex/component-adapter-interfaces';
-import {Spinner} from '@momentum-ui/react';
+import Spinner from '../generic/Spinner/Spinner';
 
 import Icon from '../generic/Icon/Icon';
 import {
