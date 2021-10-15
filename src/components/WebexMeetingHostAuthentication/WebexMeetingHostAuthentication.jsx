e617f1ebb583679214bea4d0c176df6a0e377977
Karina Sigartau <karina.sigartau0711@gmail.com>
2021-10-15 16:20:16 +0300

    fix(WebexMeetingHostAuthentication): remove name input and start meeting button from host form

--- src/components/WebexMeetingHostAuthentication/WebexMeetingHostAuthentication.jsx
+++ src/components/WebexMeetingHostAuthentication/WebexMeetingHostAuthentication.jsx
@@ -1,31 +1,21 @@
-import React, {useState, useContext} from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import webexComponentClasses from '../helpers';
-import {Button, InputField} from '../generic';
-import {useMeeting} from '../hooks';
-import {AdapterContext} from '../hooks/contexts';
+import {InputField} from '../generic';
 
 /**
  * Webex Meeting Host Authentication component
  *
  * @param {object} props  Data passed to the component
  * @param {string} props.className  Custom CSS class to apply
- * @param {string} props.meetingID  ID of the meeting
  * @param {object} props.style  Custom style to apply
  * @returns {object} JSX of the component
  *
  */
-export default function WebexMeetingHostAuthentication({className, meetingID, style}) {
-  const [name, setName] = useState();
+export default function WebexMeetingHostAuthentication({className, style}) {
   const [hostKey, setHostKey] = useState('');
-  const {ID} = useMeeting(meetingID);
-  const adapter = useContext(AdapterContext);
 
   const [cssClasses, sc] = webexComponentClasses('meeting-host-authentication', className);
-
-  const joinMeeting = () => {
-    adapter.meetingsAdapter.joinMeeting(ID, {name, hostKey});
-  };
 
   return (
     <div className={cssClasses} style={style}>
@@ -36,11 +26,6 @@ export default function WebexMeetingHostAuthentication({className, meetingID, st
       <form className={sc('form-content')}>
         {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
         <label>
-          <span>Your name</span>
-          <InputField type="text" name="name" value={name} onChange={(value) => setName(value)} />
-        </label>
-        {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-        <label>
           <span>Host key (required)</span>
           <InputField
             type="password"
@@ -49,7 +34,6 @@ export default function WebexMeetingHostAuthentication({className, meetingID, st
             onChange={(value) => setHostKey(value)}
           />
         </label>
-        <Button type="primary" onClick={joinMeeting}>Start Meeting</Button>
       </form>
     </div>
   );
@@ -57,7 +41,6 @@ export default function WebexMeetingHostAuthentication({className, meetingID, st
 
 WebexMeetingHostAuthentication.propTypes = {
   className: PropTypes.string,
-  meetingID: PropTypes.string.isRequired,
   style: PropTypes.shape(),
 };
 
