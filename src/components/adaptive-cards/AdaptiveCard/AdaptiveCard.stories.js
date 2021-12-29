7d583288a6e3604dfd4599f89a7c01b070c6ac8a
Karina Sigartau <karina.sigartau0711@gmail.com>
2021-12-29 15:17:24 +0200

    feat(AdaptiveCard): configure templating support

--- src/components/adaptive-cards/AdaptiveCard/AdaptiveCard.stories.js
+++ src/components/adaptive-cards/AdaptiveCard/AdaptiveCard.stories.js
@@ -98,7 +98,8 @@ const exampleAdaptiveCard = {
             },
             {
               type: 'TextBlock',
-              text: 'Mac, Windows, Web',
+              // eslint-disable-next-line no-template-curly-in-string
+              text: '${supportedOS}',
               weight: 'Lighter',
               color: 'Light',
               spacing: 'Small',
@@ -1705,47 +1706,50 @@ const exampleActionSet = {
 
 export const Card = Template.bind({});
 Card.args = {
-  data: exampleAdaptiveCard,
+  template: exampleAdaptiveCard,
+  context: {
+    supportedOS: 'Mac, Windows, Web',
+  },
 };
 
 export const TextBlock = Template.bind({});
 TextBlock.args = {
-  data: exampleTextBlock,
+  template: exampleTextBlock,
 };
 
 export const Column = Template.bind({});
 Column.args = {
-  data: exampleColumn,
+  template: exampleColumn,
 };
 
 export const Container = Template.bind({});
 Container.args = {
-  data: exampleContainer,
+  template: exampleContainer,
 };
 
 export const ImageSet = Template.bind({});
 ImageSet.args = {
-  data: exampleImageSet,
+  template: exampleImageSet,
 };
 
 export const Image = Template.bind({});
 Image.args = {
-  data: exampleImage,
+  template: exampleImage,
 };
 
 export const ColumnSet = Template.bind({});
 ColumnSet.args = {
-  data: exampleColumnSet,
+  template: exampleColumnSet,
 };
 
 export const RichTextBlock = Template.bind({});
 RichTextBlock.args = {
-  data: exampleRichTextBlock,
+  template: exampleRichTextBlock,
 };
 
 export const FactSet = Template.bind({});
 FactSet.args = {
-  data: exampleFactSet,
+  template: exampleFactSet,
 };
 
 export const ActionSet = Template.bind({});
