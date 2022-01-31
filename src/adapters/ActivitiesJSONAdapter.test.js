52a1603356aeafe41284d944ef1ce73eb9ea467f
Karina Sigartau <karina.sigartau0711@gmail.com>
2022-01-31 18:16:54 +0200

    feat(ActivitiesJsonAdapter): implement postAction() and use it in useActivity hook

--- src/adapters/ActivitiesJSONAdapter.test.js
+++ src/adapters/ActivitiesJSONAdapter.test.js
@@ -1,53 +1,89 @@
 import {isObservable} from 'rxjs';
-
+import {last} from 'rxjs/operators';
 import activities from '../data/activities';
 import ActivitiesJSONAdapter from './ActivitiesJSONAdapter';
 
-describe('Activities JSON Adapter Interface', () => {
-  let activityID;
+describe('Activities JSON Adapter', () => {
+  const activityID = 'activity9';
   let activitiesJSONAdapter;
+  let testActivity;
 
   beforeEach(() => {
-    [activityID] = Object.keys(activities);
-    activitiesJSONAdapter = new ActivitiesJSONAdapter(activities);
+    const mockActivitiesString = JSON.stringify(activities);
+    const mockActivitiesCopy = JSON.parse(mockActivitiesString);
+
+    activitiesJSONAdapter = new ActivitiesJSONAdapter(mockActivitiesCopy);
+    testActivity = mockActivitiesCopy[activityID];
   });
 
   afterEach(() => {
-    activityID = null;
     activitiesJSONAdapter = null;
+    testActivity = null;
   });
 
-  test('getActivity() returns an observable', () => {
-    expect(isObservable(activitiesJSONAdapter.getActivity())).toBeTruthy();
-  });
+  describe('getActivity()', () => {
+    test('returns an observable', () => {
+      expect(isObservable(activitiesJSONAdapter.getActivity())).toBeTruthy();
+    });
 
-  test('getActivity() returns an activity', (done) => {
-    activitiesJSONAdapter.getActivity(activityID).subscribe((data) => {
-      expect(data).toEqual(activities[activityID]);
-      done();
+    test('emits an Activity object on subscription', (done) => {
+      activitiesJSONAdapter.getActivity(activityID).subscribe((data) => {
+        expect(data).toEqual(testActivity);
+        done();
+      });
+    });
+
+    test('throws a proper error message when activity doesn\'t exist', (done) => {
+      const wrongActivityID = 'wrongActivityID';
+
+      activitiesJSONAdapter.getActivity(wrongActivityID).subscribe(
+        () => {
+          done.fail('Emits activity instead of returning error');
+        },
+        (error) => {
+          expect(error.message).toBe(`Could not find activity with ID "${wrongActivityID}"`);
+          done();
+        },
+      );
+    });
+
+    test('completes the observable', (done) => {
+      activitiesJSONAdapter.getActivity(activityID).subscribe(
+        () => {},
+        () => {},
+        () => {
+          expect(true).toBeTruthy();
+          done();
+        },
+      );
     });
   });
 
-  test('getActivity() throws a proper error message when activity doesn\'t exist', (done) => {
-    const wrongActivityID = 'wrongActivityID';
+  describe('postAction()', () => {
+    const inputs = {
+      firstName: 'Simon',
+      lastName: 'Damiano',
+    };
 
-    activitiesJSONAdapter.getActivity(wrongActivityID).subscribe(
-      () => {},
-      (error) => {
-        expect(error.message).toBe(`Could not find activity with ID "${wrongActivityID}"`);
-        done();
-      },
-    );
-  });
+    test('returns an observable', () => {
+      expect(isObservable(activitiesJSONAdapter.postAction())).toBeTruthy();
+    });
 
-  test('getActivity() completes the observable', (done) => {
-    activitiesJSONAdapter.getActivity(activityID).subscribe(
-      () => {},
-      () => {},
-      () => {
-        expect(true).toBeTruthy();
+    test('emits the posted action object', (done) => {
+      activitiesJSONAdapter.postAction(activityID, inputs).pipe(last()).subscribe((action) => {
+        expect(action).toMatchObject(testActivity.actions.slice(-1)[0]);
         done();
-      },
-    );
+      });
+    });
+
+    test('emits an error on invalid activity ID', (done) => {
+      activitiesJSONAdapter.postAction('activity10', {}).subscribe(
+        () => {},
+        (error) => {
+          expect(error.message).toBe('Unable to create an attachment action for activity with id "activity10"');
+          done();
+        },
+      );
+    });
   });
 });
