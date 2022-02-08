7a4ff7b9fd982ff11ae53c04d76c520888f17372
Mihai Cernaianu <mihai.cernaianu@endava.com>
2022-02-08 15:35:20 +0200

    feat(AdaptiveCardsTextBlock): add markdown support

---
+++ src/components/adaptive-cards/Markdown/Markdown.jsx
@@ -0,0 +1,43 @@
+import React, {useMemo} from 'react';
+import PropTypes from 'prop-types';
+import MarkdownIt from 'markdown-it';
+import webexComponentClasses from '../../helpers';
+
+/**
+ * Markdown component
+ *
+ * @param {object} props  Data passed to the component
+ * @param {string} props.children  Text to be converted
+ * @param {string} [props.className]  Custom CSS class to apply
+ * @returns {object} JSX of the element
+ */
+export default function Markdown({children, className}) {
+  const [cssClasses] = webexComponentClasses('markdown', className);
+  const markdownIt = useMemo(() => new MarkdownIt('zero').enable(
+    [
+      'emphasis',
+      'escape',
+      'link',
+      'list',
+      'newline',
+      'normalize',
+      'paragraph',
+      'strikethrough',
+    ],
+  ), []);
+  const html = markdownIt.render(children);
+
+  return (
+    <div className={cssClasses} dangerouslySetInnerHTML={{__html: html}} />
+  );
+}
+
+Markdown.propTypes = {
+  children: PropTypes.string,
+  className: PropTypes.string,
+};
+
+Markdown.defaultProps = {
+  children: '',
+  className: '',
+};
