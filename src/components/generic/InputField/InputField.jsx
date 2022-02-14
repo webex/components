6955c5cf436e04a3ef35583694d16df33d942d62
Daniel Coroain <danielcoroian15@gmail.com>
2022-02-14 15:57:52 +0200

    feat(inputs): create TextInput component

--- src/components/generic/InputField/InputField.jsx
+++ src/components/generic/InputField/InputField.jsx
@@ -1,9 +1,9 @@
 import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import webexComponentClasses from '../../helpers';
-import Button from '../Button/Button';
 import {useRef, useAutoFocus} from '../../hooks';
 import {clamp} from '../../../util';
+import Button from '../Button/Button';
 import Icon from '../Icon/Icon';
 import Label from '../../inputs/Label/Label';
 
@@ -13,57 +13,61 @@ const HINTS = {
 };
 
 /**
- * Input Field component.
+ * Generic input field component for building more specific components (text, date, number, etc.).
+ * Should not be used directly in forms.
  *
  * @param {object} props  Data passed to the component
+ * @param {string} [props.ariaLabel]  Aria label
  * @param {boolean} [props.autoFocus=false]  Flag indicating if the input should have autoFocus
  * @param {string} [props.className]  Custom CSS class to apply
- * @param {object} [props.style]  Custom style to apply
- * @param {string} [props.type=text]  Input type
- * @param {string} [props.name]  Input name
- * @param {(string|number)} [props.value]  Input value
- * @param {string} [props.placeholder]  Input placeholder
- * @param {number} [props.max]  Maximum value for the input element
- * @param {number} [props.min]  Minimum value for the input element
- * @param {number} [props.maxLength]  Maximum number of characters allowed
- * @param {string} [props.pattern]  Specifies a regular expression that the element's value is checked against
- * @param {Function} [props.onChange]  Action to perform on input change
  * @param {boolean} [props.disabled=false]  Flag indicating input disabled
  * @param {string} [props.error]  Error text
  * @param {string} [props.label]  Label text
- * @param {string} [props.ariaLabel]  Hint to be displayed as aria-label
+ * @param {number} [props.max]  Maximum value for the input element
+ * @param {number} [props.maxLength]  Maximum number of characters allowed
+ * @param {number} [props.min]  Minimum value for the input element
+ * @param {string} [props.name]  Input name
+ * @param {Function} [props.onChange]  Action to perform on input change
+ * @param {string} [props.pattern]  Specifies a regular expression that the element's value is checked against
+ * @param {string} [props.placeholder]  Input placeholder
  * @param {boolean} [props.required=false]  Flag indicating input required
+ * @param {boolean|React.JSX.Element} [props.rightIcon]  Icon to be displayed on the right side of the input
+ * @param {object} [props.style]  Custom style to apply
  * @param {number} [props.tabIndex]  Value of the tabIndex
+ * @param {string} [props.type=text]  Input type
+ * @param {(string|number)} [props.value]  Input value
  * @returns {object} JSX of the component
  */
 export default function InputField({
+  ariaLabel,
+  autoFocus,
   className,
-  style,
-  type,
-  name,
-  value,
-  placeholder,
-  maxLength,
-  pattern,
-  onChange,
-  max,
-  min,
   disabled,
   error,
   label,
-  ariaLabel,
+  max,
+  maxLength,
+  min,
+  name,
+  onChange,
+  pattern,
+  placeholder,
   required,
-  autoFocus,
+  rightIcon,
+  style,
   tabIndex,
+  type,
+  value,
 }) {
-  const [cssClasses, sc] = webexComponentClasses('input-field', className, {error});
+  const [cssClasses, sc] = webexComponentClasses('input-field', className, {
+    error,
+    'has-right-icon': rightIcon !== undefined,
+  });
   const [isPwdRevealed, setIsPwdRevealed] = useState(false);
   const inputRef = useRef();
-
   const handleChange = (event) => onChange(event.target.value);
   const handleIncrement = () => onChange(clamp(Number(value) + 1, min, max));
   const handleDecrement = () => onChange(clamp(Number(value) - 1, min, max));
-  const clearInput = () => onChange('');
 
   const toggleIsPwdRevealed = () => {
     setIsPwdRevealed((revealed) => !revealed);
@@ -81,29 +85,29 @@ export default function InputField({
     >
       <div className={sc('form-control')}>
         <input
-          type={isPwdRevealed ? 'text' : type}
-          value={value}
-          name={name}
-          className={sc('input')}
-          placeholder={placeholder}
-          max={max}
-          min={min}
-          maxLength={maxLength}
-          pattern={pattern}
-          onChange={handleChange}
-          disabled={disabled}
           aria-label={ariaLabel}
-          required={required}
           // disabling no-autofocus because otherwise this element cannot be autofocused depending on the autoFocus prop
           // eslint-disable-next-line jsx-a11y/no-autofocus
           autoFocus={autoFocus}
-          tabIndex={tabIndex}
+          className={sc('input')}
+          disabled={disabled}
+          max={max}
+          maxLength={maxLength}
+          min={min}
+          name={name}
+          onChange={handleChange}
+          pattern={pattern}
+          placeholder={placeholder}
           ref={inputRef}
+          required={required}
+          tabIndex={tabIndex}
+          type={isPwdRevealed ? 'text' : type}
+          value={value}
         />
         {type === 'password' && value && (
           <Button
             type="ghost"
-            className={sc('input-field-right-icon')}
+            className={sc('right-icon')}
             size={28}
             onClick={toggleIsPwdRevealed}
             tabIndex={tabIndex}
@@ -112,10 +116,10 @@ export default function InputField({
             <Icon name={isPwdRevealed ? 'hide-password' : 'show-password'} />
           </Button>
         )}
-        {type !== 'password' && value !== undefined && (
-          <Button type="ghost" className={sc('input-field-right-icon')} size={28} onClick={clearInput}>
-            <Icon name="cancel" size={16} />
-          </Button>
+        {!!rightIcon && (
+          <span className={sc('right-icon')}>
+            {rightIcon}
+          </span>
         )}
         {type === 'number' && (
           <div className={sc('input-controls')}>
@@ -133,46 +137,51 @@ export default function InputField({
 }
 
 InputField.propTypes = {
+  ariaLabel: PropTypes.string,
   autoFocus: PropTypes.bool,
   className: PropTypes.string,
+  disabled: PropTypes.bool,
+  error: PropTypes.string,
+  label: PropTypes.string,
+  max: PropTypes.number,
+  maxLength: PropTypes.number,
+  min: PropTypes.number,
+  name: PropTypes.string,
+  onChange: PropTypes.func,
+  pattern: PropTypes.string,
+  placeholder: PropTypes.string,
+  required: PropTypes.bool,
+  rightIcon: PropTypes.oneOfType([
+    PropTypes.bool,
+    PropTypes.object,
+  ]),
   style: PropTypes.shape(),
+  tabIndex: PropTypes.number,
   type: PropTypes.string,
   value: PropTypes.oneOfType([
     PropTypes.string,
     PropTypes.number,
   ]),
-  name: PropTypes.string,
-  placeholder: PropTypes.string,
-  maxLength: PropTypes.number,
-  pattern: PropTypes.string,
-  onChange: PropTypes.func,
-  max: PropTypes.number,
-  min: PropTypes.number,
-  disabled: PropTypes.bool,
-  error: PropTypes.string,
-  label: PropTypes.string,
-  ariaLabel: PropTypes.string,
-  required: PropTypes.bool,
-  tabIndex: PropTypes.number,
 };
 
 InputField.defaultProps = {
+  ariaLabel: undefined,
   autoFocus: false,
   className: undefined,
-  style: undefined,
-  type: 'text',
-  value: undefined,
-  name: undefined,
-  placeholder: undefined,
-  maxLength: undefined,
-  pattern: undefined,
-  onChange: undefined,
-  max: undefined,
-  min: undefined,
   disabled: false,
   error: undefined,
   label: undefined,
-  ariaLabel: undefined,
+  max: undefined,
+  maxLength: undefined,
+  min: undefined,
+  name: undefined,
+  onChange: undefined,
+  pattern: undefined,
+  placeholder: undefined,
   required: false,
+  rightIcon: undefined,
+  style: undefined,
   tabIndex: 0,
+  type: 'text',
+  value: undefined,
 };
