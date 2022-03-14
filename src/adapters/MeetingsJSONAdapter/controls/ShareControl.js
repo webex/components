8acf8cc6d7329963d7717e5b692780c9c47ea61a
Karina Sigartau <karina.sigartau0711@gmail.com>
2022-03-14 15:02:58 +0200

    feat(ShareControl): disable share screen on mobile browsers and Internet Explorer browser

--- src/adapters/MeetingsJSONAdapter/controls/ShareControl.js
+++ src/adapters/MeetingsJSONAdapter/controls/ShareControl.js
@@ -1,4 +1,4 @@
-import {Observable} from 'rxjs';
+import {Observable, of} from 'rxjs';
 import {map, distinctUntilChanged} from 'rxjs/operators';
 import {MeetingControlState} from '@webex/component-adapter-interfaces';
 import MeetingControl from './MeetingControl';
@@ -43,8 +43,18 @@ export default class ShareControl extends MeetingControl {
       text: 'Stop sharing',
       tooltip: 'Stop sharing content',
     };
+    const notSupported = {
+      ID: this.ID,
+      type: 'TOGGLE',
+      state: MeetingControlState.DISABLED,
+      icon: 'share-screen-presence-stroke',
+      text: 'Start sharing',
+      tooltip: 'Share screen not supported',
+    };
 
-    return this.adapter.getMeeting(meetingID).pipe(
+    const browserSupport = navigator.mediaDevices && navigator.mediaDevices.getDisplayMedia;
+
+    return !browserSupport ? of(notSupported) : this.adapter.getMeeting(meetingID).pipe(
       map((meeting) => !!meeting.localShare.stream),
       distinctUntilChanged(),
       map((localShareExists) => (localShareExists ? active : inactive)),
