bf68b7ae5cd1d534ba96b93a29fe13d885224065
Karina Sigartau <karina.sigartau0711@gmail.com>
2022-03-18 15:41:43 +0200

    feat(WebexAdaptiveCard): add onSubmit prop to notify when submit starts and ends

--- src/components/WebexAdaptiveCard/WebexAdaptiveCard.jsx
+++ src/components/WebexAdaptiveCard/WebexAdaptiveCard.jsx
@@ -6,23 +6,43 @@ import {useAdaptiveCard} from '../hooks';
 import AdaptiveCard from '../adaptive-cards/AdaptiveCard/AdaptiveCard';
 
 /**
+ * Action to perform when submitting a card
+ *
+ * @callback submitCallback
+ * @param {object} inputs  Data to submit
+ * @param {Promise<object>} submitPromise  Promise that resolves to the submitted action
+ */
+
+/**
  * WebexAdaptiveCard component displays a webex adaptive card.
  *
  * @param {object} props  Data passed to the component
  * @param {string} props.activityID  ID of the activity corresponding to this card
  * @param {string} [props.className]  Custom CSS class to apply
+ * @param {submitCallback} [props.onSubmit]  Action to perform when submitting a card
  * @param {object} [props.style]  Custom style to apply
  * @returns {object} JSX of the component
  */
-export default function WebexAdaptiveCard({activityID, className, style}) {
+export default function WebexAdaptiveCard({
+  activityID,
+  className,
+  onSubmit,
+  style,
+}) {
   const [card, submit] = useAdaptiveCard(activityID);
   const [cssClasses] = webexComponentClasses('adaptive-card', className);
+
+  const handleSubmit = (inputs) => {
+    const submitPromise = submit(inputs).toPromise();
+
+    onSubmit(submitPromise, inputs);
+  };
 
   return (
     <AdaptiveCard
       className={cssClasses}
       template={card}
-      onSubmit={(inputs) => submit(inputs).subscribe()}
+      onSubmit={handleSubmit}
       style={style}
     />
   );
@@ -32,9 +52,11 @@ WebexAdaptiveCard.propTypes = {
   activityID: PropTypes.string.isRequired,
   className: PropTypes.string,
   style: PropTypes.shape(),
+  onSubmit: PropTypes.func,
 };
 
 WebexAdaptiveCard.defaultProps = {
   className: undefined,
   style: undefined,
+  onSubmit: () => {},
 };
