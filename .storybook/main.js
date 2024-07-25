adb3011ec38545576de0b1ec5eb69d7bc4058122
Priya <pkesari@cisco.com>
2024-07-25 17:09:41 +0530

    fix(tooling): upgraded webpack version to be used for storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -9,4 +11,17 @@ module.exports = {
     "@storybook/preset-scss",
     "storybook-addon-themes",
   ],
+
+  framework: '@storybook/react',
+  core: {
+    builder: 'webpack5',
+  },
+
+  webpackFinal: async (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@webex/component-adapter-interfaces': path.resolve(__dirname, '../node_modules/@webex/component-adapter-interfaces/dist/webex-component-adapter-interfaces.es'),
+    };
+    return config;
+  },
 }
